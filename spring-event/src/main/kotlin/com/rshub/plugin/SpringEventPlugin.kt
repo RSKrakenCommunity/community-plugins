87d504e74409942c7164b12b0aff4ebf72801266
David <david.schlachter@mindmatrix.net>
2022-04-14 18:16:17 -0700

    Added support for agility and prayer.

--- spring-event/src/main/kotlin/com/rshub/plugin/SpringEventPlugin.kt
+++ spring-event/src/main/kotlin/com/rshub/plugin/SpringEventPlugin.kt
@@ -11,35 +11,68 @@ import com.rshub.api.skills.Skill
 import com.rshub.api.world.WorldHelper
 import kraken.plugin.api.Debug
 import kraken.plugin.api.ImGui
+import kraken.plugin.api.Players
 import kraken.plugin.api.Widgets
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.DataInputStream
+import java.io.DataOutputStream
 
 class SpringEventPlugin : KotlinPlugin("Spring Event") {
 
     private val skill: Skill
         get() {
-            val value = CacheHelper.getVarbitValue(49157)
-            return if (value == 1) {
-                Skill.COOKING
-            } else Skill.HERBLORE
+            return when(selectedSkill) {
+                0, 1 -> {
+                    val value = CacheHelper.getVarbitValue(49157)
+                    if(value == 1) Skill.COOKING else Skill.HERBLORE
+                }
+                2, 3 -> {
+                    val value = CacheHelper.getVarbitValue(49158)
+                    if(value == 1) Skill.PRAYER else Skill.AGILITY
+                }
+                else -> Skill.HERBLORE
+            }
         }
-    private val skillOptions = arrayOf("Cooking", "Herblore")
+
+    private val rotation: Int get() = CacheHelper.getVarbitValue(49159)
+
+    private val skillOptions = arrayOf("Cooking", "Herblore", "Prayer", "Agility")
     private var selectedSkill = 0
 
     private var currentCauldronId = getCauldron()?.id ?: -1
 
+    @Transient var start: Boolean = false
+
     override suspend fun loop(): Int {
+        if(!start) {
+            return 600
+        }
         selectSkill()
-        val obj = getCauldron()
-        if (obj != null && obj.interact(ObjectAction.OBJECT1)) {
-            currentCauldronId = obj.id
-            delayUntil(300000) {
-                currentCauldronId != getCauldron()?.id
+        when (skill) {
+            Skill.AGILITY, Skill.PRAYER -> {
+                updateRotation()
+                val plr = Players.self()
+                if (plr != null && !plr.isMoving) {
+                    WorldHelper.closestObjectIgnoreClip { it.id == 119638 }?.interact(ObjectAction.OBJECT1)
+                    delayUntil { Players.self()?.isMoving ?: true }
+                }
             }
+            Skill.HERBLORE, Skill.COOKING -> {
+                val obj = getCauldron()
+                if (obj != null && obj.interact(ObjectAction.OBJECT1)) {
+                    currentCauldronId = obj.id
+                    delayUntil(300000) {
+                        currentCauldronId != getCauldron()?.id
+                    }
+                }
+            }
+            else -> { Debug.log("Unsupported Skill ${skill.name}") }
         }
         return 600
     }
 
-    private fun getDirection(): String {
+    private fun getCauldronDirection(): String {
         if (Widgets.isOpen(596)) {
             val text = Widgets.getGroupById(596).getWidget(0).getChild(1).text
             Debug.log(text)
@@ -50,8 +83,34 @@ class SpringEventPlugin : KotlinPlugin("Spring Event") {
         return "south"
     }
 
+    private fun getRotation(): String {
+        if (Widgets.isOpen(596)) {
+            val text = Widgets.getGroupById(596).getWidget(0).getChild(1).text
+            return text.lowercase().split(":")[1].trim()
+        }
+        return "clockwise"
+    }
+
+    private suspend fun updateRotation() {
+        val rot = rotation
+        when (getRotation()) {
+            "clockwise" -> {
+                if (rot == 1) {
+                    ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059478)
+                    delayUntil { rotation == 0 }
+                }
+            }
+            "anti-clockwise" -> {
+                if (rot == 0) {
+                    ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059483)
+                    delayUntil { rotation == 1 }
+                }
+            }
+        }
+    }
+
     private fun getCauldron(): WorldObject? {
-        return when (getDirection()) {
+        return when (getCauldronDirection()) {
             "north" -> WorldHelper.closestObject { it.id == 119626 }
             "south" -> WorldHelper.closestObject { it.id == 119628 }
             "east" -> WorldHelper.closestObject { it.id == 119627 }
@@ -61,21 +120,58 @@ class SpringEventPlugin : KotlinPlugin("Spring Event") {
 
     private suspend fun selectSkill() {
         if (Widgets.isOpen(596)) {
-            if(selectedSkill == 0 && skill === Skill.HERBLORE) {
+            if (selectedSkill == 0 && skill === Skill.HERBLORE) {
                 ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059472)
                 delayUntil { skill === Skill.COOKING }
-            } else if(selectedSkill == 1 && skill == Skill.COOKING) {
+            } else if (selectedSkill == 1 && skill == Skill.COOKING) {
                 ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059468)
                 delayUntil { skill === Skill.HERBLORE }
+            } else if (selectedSkill == 2 && skill === Skill.AGILITY) {
+                ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059472)
+                delayUntil { skill === Skill.PRAYER }
+            } else if(selectedSkill == 3 && skill === Skill.PRAYER) {
+                ActionHelper.menu(MenuAction.WIDGET, 1, -1, 39059468)
+                delayUntil { skill === Skill.AGILITY }
             }
         }
     }
 
+    private fun loadData() {
+        val stream = ByteArrayInputStream(context.persistentData)
+        val input = DataInputStream(stream)
+        selectedSkill = input.readUnsignedByte()
+        start = input.readBoolean()
+    }
+
+    private fun writeData() {
+        val stream = ByteArrayOutputStream()
+        val out = DataOutputStream(stream)
+        out.writeByte(selectedSkill)
+        out.writeBoolean(start)
+        context.persistentData = stream.toByteArray()
+    }
+
     override fun onLoad() {
+        loadData()
     }
 
     override suspend fun paint() {
-        selectedSkill = ImGui.combo("Skill", skillOptions, selectedSkill)
+        if(start) {
+            if(ImGui.button("Stop")) {
+                start = false
+                writeData()
+            }
+        } else {
+            if(ImGui.button("Start")) {
+                start = true
+                writeData()
+            }
+        }
+        val selectedSkill = ImGui.combo("Skill", skillOptions, selectedSkill)
+        if (selectedSkill != this.selectedSkill) {
+            this.selectedSkill = selectedSkill
+            writeData()
+        }
     }
 
     override suspend fun paintOverlay() {
